82d6f34d821ea9a06ca505e469985af17eaaf825
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-10 06:52:52 -0300

    add header and using css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
 function App() {
+    function handleRestartGame() {
+        alert("Reiniciar o jogo!");
+    }
+
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className={styles.container}>
+            <main>
+                <Header
+                    current={5}
+                    max={10}
+                    onRestart={handleRestartGame}
+                ></Header>
+            </main>
         </div>
     );
 }
