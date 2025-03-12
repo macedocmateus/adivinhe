6787fac817fdc2f1d735f932a9a851695ce1f890
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-12 06:13:32 -0300

    add tip component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+
 function App() {
     function handleRestartGame() {
         alert("Reiniciar o jogo!");
@@ -13,6 +15,7 @@ function App() {
                     max={10}
                     onRestart={handleRestartGame}
                 ></Header>
+                <Tip tip="Uma das linguagens de programação mais utilizadas"></Tip>
             </main>
         </div>
     );
