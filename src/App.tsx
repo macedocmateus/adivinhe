49cbea6b1e759cbefc224209235185ef2a544b2e
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-12 11:18:39 -0300

    add main styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 
 function App() {
     function handleRestartGame() {
@@ -15,7 +16,16 @@ function App() {
                     max={10}
                     onRestart={handleRestartGame}
                 ></Header>
+
                 <Tip tip="Uma das linguagens de programação mais utilizadas"></Tip>
+
+                <div className={styles.word}>
+                    <Letter value="R"></Letter>
+                    <Letter value="E"></Letter>
+                    <Letter value="A"></Letter>
+                    <Letter value="C"></Letter>
+                    <Letter value="T"></Letter>
+                </div>
             </main>
         </div>
     );
