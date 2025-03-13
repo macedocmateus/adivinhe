06a7a0799d4e470075adcd3d539380a3271f3e12
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-13 05:56:14 -0300

    add sorting word

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,39 @@ import { Input } from "./components/Input";
 import { Button } from "./components/Button";
 import { LettersUsed } from "./components/LettersUsed";
 
+import { WORDS, Challenge } from "./utils/words";
+
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
+    const [attempts, setAttempts] = useState(0);
+    const [letter, setLetter] = useState("");
+    const [challenge, setChallenge] = useState<Challenge | null>(null);
+
     function handleRestartGame() {
         alert("Reiniciar o jogo!");
     }
+
+    function startGame() {
+        // Math floor pega o numero inteiro
+        const index = Math.floor(Math.random() * WORDS.length);
+        const randomWord = WORDS[index];
+        setChallenge(randomWord);
+
+        setAttempts(0);
+        setLetter("");
+    }
+
+    useEffect(() => {
+        startGame();
+    }, []);
 
     return (
         <div className={styles.container}>
             <main>
                 <Header
-                    current={5}
+                    current={attempts}
                     max={10}
                     onRestart={handleRestartGame}
                 ></Header>
