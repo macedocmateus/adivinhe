2a794a35840b3a67c83ac90a573cfa171d64e140
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-03-13 05:07:03 -0300

    add letters sizes variations

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,16 @@
 import styles from "./styles.module.css";
 
+import { Letter } from "../Letter";
+
 export function LettersUsed() {
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras utilizadas</h5>
+
+            <div>
+                <Letter value="R" size="small"></Letter>
+                <Letter value="X" size="small"></Letter>
+            </div>
         </div>
     );
 }
